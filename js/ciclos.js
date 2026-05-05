a39cc6514bd6a67da11de1e1b643595aa34daf4d
Juan Melo <jcmelos@unal.edu.co>
2026-05-04 20:14:29 -0500

    Se agrega todo lo de ciclos

---
+++ js/ciclos.js
@@ -0,0 +1,22 @@
+let edades=[18,30,40,35,22,50];
+
+let frutas=["manzana", "pera","piña","mora","banano"];
+
+
+for(a in frutas){
+    console.log(frutas[a])
+}
+
+let i=0
+while(i<edades.length){
+    console.log(edades[i]);
+    i++;
+}
+
+let p=0;
+
+do{
+    console.log(frutas[p]);
+    p++;
+
+} while(p<frutas.length)
